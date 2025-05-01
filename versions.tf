4e1809d19468823c212d39cd3fd7f161c4f453b7
WestonPlatter <weston.platter@masterpoint.io>
2025-05-01 16:31:54 -0600

    add user and group

--- versions.tf
+++ versions.tf
@@ -2,9 +2,13 @@ terraform {
   required_version = ">= 1.0"
 
   required_providers {
+    googleworkspace = {
+      source  = "hashicorp/googleworkspace"
+      version = ">= 0.7.0"
+    }
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.0"
+      version = ">= 3.0, < 4.0"
     }
   }
 }
