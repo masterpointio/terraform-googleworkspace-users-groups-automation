3e9533285b991ae9a36a90be1e1d407a7df9953c
WestonPlatter <weston.platter@masterpoint.io>
2025-05-05 11:34:54 -0600

    examples: update example to have groups

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -7,6 +7,19 @@ module "googleworkspace" {
       family_name   = "Last"
       given_name    = "First"
       password      = "insecure-password-for-example" # trunk-ignore(checkov/CKV_SECRET_6)
+      groups = {
+        "platform" = {
+          role = "MEMBER"
+        }
+      }
+    }
+  }
+
+  groups = {
+    "platform" = {
+      name        = "Platform"
+      email       = "platform@example.com"
+      description = "Platform team description"
     }
   }
 }
