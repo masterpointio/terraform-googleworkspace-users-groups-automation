4b931324bfd5d5831cf56548222db3bcae345bee
WestonPlatter <weston.platter@masterpoint.io>
2025-05-06 10:05:28 -0600

    fix: bring back the full part of the example

--- examples/complete/providers.tf
+++ examples/complete/providers.tf
@@ -2,10 +2,10 @@ provider "googleworkspace" {
   # # use the 'my_customer' string, which is an alias that Google's API recognizes to reference your account's customerId.
   # # Custom Schemas on the user object will fail if the customer_id is set to your actual customer_id.
   # # For more details see: https://developers.google.com/workspace/admin/directory/reference/rest/v1/schemas/get
-  # customer_id = "my_customer"
+  customer_id = "my_customer"
 
-  # credentials             = "/Users/my_user/Downloads/my-google-project-credentials-1234567890.json"
-  # impersonated_user_email = "my_impersonated_user_email@my_domain.com"
+  credentials             = "/Users/my_user/Downloads/my-google-project-credentials-1234567890.json"
+  impersonated_user_email = "my_impersonated_user_email@my_domain.com"
 
   oauth_scopes = [
     "https://www.googleapis.com/auth/admin.directory.group",
